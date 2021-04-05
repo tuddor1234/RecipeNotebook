6e1ec15b6a8fb063001bd6e8512cd74f15ff9908
tuddor1234 <tuddor1234@yahoo.com>
2021-04-05 20:41:52 +0300

    init

---
+++ Solution1/RecipeNotebook/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using RecipeNotebook.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RecipeNotebook
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            GetAllRecipes();
+
+        }
+
+        private async void GetAllRecipes()
+        {
+            HttpClient client = new HttpClient();
+
+            var response =await client.GetStringAsync("https://reciperestapi.azurewebsites.net/api/recipe");
+
+            var recipes = JsonConvert.DeserializeObject<List<Recipe>>(response);
+
+            RecipeListView.ItemsSource = recipes; 
+        }
+    }
+}
---
+++ Solution1/RecipeNotebook/Models/Recipe.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipeNotebook.Models
+{
+    class Recipe
+    {
+        public string Name { get; set; }
+        public string fileURL { get; set; }
+    }
+}
